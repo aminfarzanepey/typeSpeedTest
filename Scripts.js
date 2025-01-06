b693d73628b7d1c5d83d09c862be79f98a23d45e
Amin Farzanepey <aminfarzanepey@gmail.com>
2025-01-06 12:06:54 +0330

    add leadingZero function that add zero before time

--- Scripts.js
+++ Scripts.js
@@ -2,8 +2,15 @@ const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0];
 
+function leadingZero(time){
+    if(time <= 9){
+        time = "0" + time;
+    }
+    return time;
+}
+
 function runTimer() {
-    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
 
     timer[3]++;
